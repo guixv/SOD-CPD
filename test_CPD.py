3772f9a2e38f8fc5b951354be0f823cee9a05d0f
Qiu Chaoyi <954182519@qq.com>
2023-06-04 16:08:12 +0800

    main

--- test_CPD.py
+++ test_CPD.py
@@ -8,22 +8,20 @@ import imageio
 
 from model.CPD_models import CPD_VGG
 from model.CPD_ResNet_models import CPD_ResNet
+from model.CPD_convnext import CPD_convnext
 from data import test_dataset
+from tqdm import tqdm
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--testsize', type=int, default=224, help='testing size')
-    parser.add_argument('--is_ResNet', type=bool, default=False, help='VGG or ResNet backbone')
     opt = parser.parse_args()
 
     dataset_path = 'path/dataset/'
 
-    if opt.is_ResNet:
-        model = CPD_ResNet()
-        model.load_state_dict(torch.load('CPD-R.pth'))
-    else:
-        model = CPD_VGG()
-        model.load_state_dict(torch.load('CPD.pth'))
+    model = CPD_ResNet()
+    model.load_state_dict(torch.load('epoch_300_CPD.pth'))
+
 
     model.cuda()
     model.eval()
@@ -31,16 +29,14 @@ if __name__ == "__main__":
     test_datasets = ['DUTS-TE']
 
     for dataset in test_datasets:
-        if opt.is_ResNet:
-            save_path = './results/ResNet50/' + dataset + '/'
-        else:
-            save_path = './results/VGG16/' + dataset + '/'
+        save_path = './results/res_300/' + dataset + '/'
         if not os.path.exists(save_path):
             os.makedirs(save_path)
         image_root = dataset_path + dataset + '/DUTS-TE-Image/'
         gt_root = dataset_path + dataset + '/DUTS-TE-Mask/'
         test_loader = test_dataset(image_root, gt_root, opt.testsize)
-        for i in range(test_loader.size):
+        # pbar = tqdm(total=test_loader.size)
+        for i in tqdm(range(test_loader.size)):
             image, gt, name = test_loader.load_data()
             gt = np.asarray(gt, np.float32)
             gt /= (gt.max() + 1e-8)
@@ -49,4 +45,7 @@ if __name__ == "__main__":
             res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
             res = res.sigmoid().data.cpu().numpy().squeeze()
             res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+            res = (res * 255.0).astype('uint8')
             imageio.imsave(save_path+name, res)
+        #     pbar.update(1)
+        # pbar.close()
--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
-
 import numpy as np
 import pdb, os, argparse
 from datetime import datetime
 from tqdm import tqdm
+import torch.optim.lr_scheduler as lr_scheduler
+import math
 
 from model.CPD_models import CPD_VGG
 from model.CPD_convnext import CPD_convnext
@@ -14,7 +15,7 @@ from data import get_loader
 from utils import clip_gradient, adjust_lr
 
 
-def train(train_loader, model, optimizer, epoch):
+def train(train_loader, model, optimizer, epoch,scheduler):
     model.train()
     for i, pack in enumerate(tqdm(train_loader), start=1):
         optimizer.zero_grad()
@@ -32,6 +33,7 @@ def train(train_loader, model, optimizer, epoch):
 
         clip_gradient(optimizer, opt.clip)
         optimizer.step()
+        scheduler.step()  #更新学习率
 
         if i == 400 or i == total_step:
             print('{} Epoch [{:03d}/{:03d}], Step [{:04d}/{:04d}], Loss1: {:.4f} Loss2: {:0.4f}'.
@@ -48,24 +50,25 @@ def train(train_loader, model, optimizer, epoch):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoch', type=int, default=300, help='epoch number')
-    parser.add_argument('--lr', type=float, default=4e-3, help='learning rate')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+    parser.add_argument("--lrf", type=int, default=0.1)
     parser.add_argument('--batchsize', type=int, default=32, help='training batch size')
     parser.add_argument('--trainsize', type=int, default=224, help='training dataset size')
     parser.add_argument('--clip', type=float, default=0.5, help='gradient clipping margin')
     parser.add_argument('--decay_rate', type=float, default=0.05, help='decay rate of learning rate')
-    parser.add_argument('--decay_epoch', type=int, default=50, help='every n epochs decay learning rate')
-    parser.add_argument('--save_path', type=str, default='models/CPD_convnext/', help='save path')
+    # parser.add_argument('--decay_epoch', type=int, default=50, help='every n epochs decay learning rate')
+    parser.add_argument('--save_path', type=str, default='models/CPD_res1/', help='save path')
     parser.add_argument("--image_path", type=str, default='data/DUTS-TR/DUTS-TR-Image/')
     parser.add_argument("--mask_path", type=str, default='data/DUTS-TR/DUTS-TR-Mask/')
     opt = parser.parse_args()
 
     print('Learning Rate: {} '.format(opt.lr))
     # build models
-    model = CPD_convnext()
+    model = CPD_ResNet()
 
     model.cuda()
     params = model.parameters()
-    # optimizer = torch.optim.Adam(params, opt.lr)
+    # optimizer = torch.optim.Adam(params, opt.lr)  
     # optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)  # 优化器
     optimizer = torch.optim.AdamW(model.parameters(), lr=opt.lr, betas=(0.9, 0.9999))
 
@@ -77,12 +80,20 @@ if __name__ == "__main__":
     gt_root = opt.mask_path
     train_loader = get_loader(image_root, gt_root, batchsize=opt.batchsize, trainsize=opt.trainsize)
     total_step = len(train_loader)
-    print("using {} images for training.".format(total_step))  # 用于打印总的训练集数量和验证集数量
+    print("using {} step for training.".format(total_step))  # 用于打印总的训练集数量和验证集数量
 
     CE = torch.nn.BCEWithLogitsLoss()
+
+    epoch = opt.epoch
+
+    lrf = opt.lrf
+    lf = lambda x: ((1 + math.cos(x * math.pi / (epoch + 1))) / 2) * (1 - lrf) + lrf  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)  # 学习率变化
 
     print("Let's go!")
 
     for epoch in range(1, opt.epoch+1):
-        adjust_lr(optimizer, opt.lr, epoch, opt.decay_rate, opt.decay_epoch)
-        train(train_loader, model, optimizer, epoch)
+        # adjust_lr(optimizer, opt.lr, epoch, opt.decay_rate, opt.decay_epoch)
+        train(train_loader, model, optimizer, epoch,scheduler)
+
+    print("======end===========")
